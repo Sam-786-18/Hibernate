1d789be65482e1610f63d6daf5ebf214233d2334
Hi there! ,Sateesh Meena <59795648+Sam-786-18@users.noreply.github.com>
2026-08-14 13:44:34 +0530

    Add files via upload

---
+++ HibernateProj006-Employee-Annotation/src/main/java/com/scjp/test/EmployeeCurdApp.java
@@ -0,0 +1,28 @@
+package com.scjp.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.scjp.entity.Employee;
+import com.scjp.hibernate.util.HibernateUtil;
+
+public class EmployeeCurdApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+SessionFactory sessionFactory=HibernateUtil.buildSessionFactory();
+Session session=sessionFactory.openSession();
+Transaction tx=session.beginTransaction();
+Employee emp=new Employee();
+emp.setEname("Minu");
+emp.setJob("Manager");
+emp.setSalary(70000.0);
+session.persist(emp);
+tx.commit();
+session.close();
+
+
+	}
+
+}
